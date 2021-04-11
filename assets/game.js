880abc44c302f834fc4fa87c4490b46bbf99640e
johng252627 <johngill252627@gmail.com>
2021-04-11 17:03:41 +0000

    Add function for changing colors in game.js

--- assets/game.js
+++ assets/game.js
@@ -1,1 +1,10 @@
-let tileArray = ["pink", "pink", "green", "green", "blue", "blue", "red", "red", "purple", "purple", "orange", "orange"]
+(function ($) {
+
+    $(div).click(function () {
+        var tileColor = ['pink', 'pink', 'green', 'green', 'blue', 'blue', 'red', 'red', 'purple', 'purple', 'orange', 'orange'];
+        $('div').each(function (i) {
+            $(this).removeClass(hidden).addClass(tileColor[Math.floor(Math.random() * tileColor.length)]);
+        });
+    });
+
+});
