03e80625faa395fb195d5fba2ba29342f07296bb
johng252627 <johngill252627@gmail.com>
2021-04-11 18:33:31 +0000

    Add click functions to change class for tiles

--- assets/game.js
+++ assets/game.js
@@ -1,10 +1,47 @@
-(function ($) {
+$("#tile1").click(function () {
+    (this).removeClass("hidden").addClass("pink");
+})
 
-    $(div).click(function () {
-        var tileColor = ['pink', 'pink', 'green', 'green', 'blue', 'blue', 'red', 'red', 'purple', 'purple', 'orange', 'orange'];
-        $('div').each(function (i) {
-            $(this).removeClass(hidden).addClass(tileColor[Math.floor(Math.random() * tileColor.length)]);
-        });
-    });
+$("#tile2").click(function () {
+    (this).removeClass("hidden").addClass("pink");
+})
 
-});
+$("#tile3").click(function () {
+    (this).removeClass("hidden").addClass("green");
+})
+
+$("#tile4").click(function () {
+    (this).removeClass("hidden").addClass("green");
+})
+
+$("#tile5").click(function () {
+    (this).removeClass("hidden").addClass("blue");
+})
+
+$("#tile6").click(function () {
+    (this).removeClass("hidden").addClass("blue");
+})
+
+$("#tile7").click(function () {
+    (this).removeClass("hidden").addClass("red");
+})
+
+$("#tile8").click(function () {
+    (this).removeClass("hidden").addClass("red");
+})
+
+$("#tile9").click(function () {
+    (this).removeClass("hidden").addClass("purple");
+})
+
+$("#tile10").click(function () {
+    (this).removeClass("hidden").addClass("purple");
+})
+
+$("#tile11").click(function () {
+    (this).removeClass("hidden").addClass("orange");
+})
+
+$("#tile12").click(function () {
+    (this).removeClass("hidden").addClass("orange");
+})
